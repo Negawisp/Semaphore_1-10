32139b45488b4f562d86222d20901f6f245eb0a1
Negawisp <31891164+Negawisp@users.noreply.github.com>
2018-12-14 15:32:27 +0300

    Works now.

--- main.c
+++ main.c
@@ -3,96 +3,61 @@
 #include <sys/sem.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <wait.h>
 #include <assert.h>
 
-#define CHILDREN_AMMOUNT 10
 
-#define CHILD	child_pid == 0
-
-#define FORK()				\
-	child_pid = fork ();		\
-	assert (child_pid >= 0);
-
-
-#define A( value_to_increase )				\
-	sembuffer.sem_num = 0;				\
-	sembuffer.sem_flg = 0;				\
-	sembuffer.sem_op  = value_to_increase;		\
-	assert (semop (semid, &sembuffer, 1) >= 0);
-
-#define D( value_to_wait )				\
-	sembuffer.sem_num = 0;				\
-	sembuffer.sem_flg = 0;				\
-	sembuffer.sem_op  = -value_to_wait;		\
-	assert (semop (semid, &sembuffer, 1) >= 0);
-
-#define Z()						\
-	sembuffer.sem_num = 0;				\
-	sembuffer.sem_flg = 0;				\
-	sembuffer.sem_op  = 0;				\
-	assert (semop (semid, &sembuffer, 1) >= 0);
-
-
-int get_sem_id (char* filename, int length)
-{
-	assert (filename);
-
-	key_t 	key = ftok (filename, 0);			assert (key >= 0);
-	int 	id  = semget (key, length, 0666|IPC_CREAT);	assert (id  >= 0);
-
-	return id;
-}
-
-int create_pipe (int* pipe_in, int* pipe_out)
-{
-	assert (pipe_in && pipe_out);
-	
-	int fd [2] = {};
-	assert (pipe (fd) == 0);
-	
-	*pipe_out = fd[0];
-	*pipe_in  = fd[1];
-
-	return 0;
+#define SEMOP( number, value, flag )			\
+{							\
+	sembuffer.sem_num = number;			\
+	sembuffer.sem_flg = flag;			\
+	sembuffer.sem_op  = value;			\
+	assert (semop (id, &sembuffer, 1) >= 0);	\
 }
 
 
 
-int main (int agrc, char* argv [], char* envp [])
-{	
-	int		semid      = get_sem_id ("dragon_scroll.txt", 1);
-	struct sembuf 	sembuffer  = {};
+int main ()
+{
+	key_t		key = ftok   ("dragon_scroll.txt", 0);
+	int		id  = semget (key, 1, 0666|IPC_CREAT);	assert (id >= 0);
+	struct sembuf 	sembuffer = {};
 
-	int pipe_in  = 0;
-	int pipe_out = 0;
-	create_pipe (&pipe_in, &pipe_out);
+	sembuffer.sem_num = 0;
+	sembuffer.sem_flg = 0;
 
+	int fd[2] = {};
+	pipe (fd);
 	int child_retvalue = 0;
-
-	pid_t	child_pid = -1;
-
+	int wstatus;
 
 	int i = 0;
-	for (i = 0; i < CHILDREN_AMMOUNT; i++)
+	int ch_pID = 0;
+
+
+	for (i = 1; i <= 10; i++)
 	{
-		FORK ();
-		if (CHILD) break;
+		ch_pID = fork();
+		assert (ch_pID >= 0);
+		
+		if (ch_pID == 0) break;
 	}
 
-	if (CHILD)
+	if (ch_pID == 0)
 	{
-		D (2*i + 1);
-		assert (write (pipe_in, &i, sizeof(int)) == sizeof(int));
-		D (1);
+		SEMOP (0, -i, 0);
+		assert (write (fd[1], &i, sizeof(int)) == sizeof(int));
 		return 0;
 	}
-	
-	for (i = 0; i < CHILDREN_AMMOUNT; i++)
-	{
-		A (2*i + 2);
-		assert (read (pipe_out, &child_retvalue, sizeof(int)) == sizeof(int));
-		printf ("%d %d\n", i, child_retvalue);
 
+	for (i = 1; i <= 10; i++)
+	{
+		SEMOP (0, i, 0);
+		assert (read (fd[0], &child_retvalue, sizeof(int)) == sizeof(int));
+		wait(&wstatus);
+		printf ("%d %d\n", i, child_retvalue);
 	}
+
+	
 	return 0;
 }
